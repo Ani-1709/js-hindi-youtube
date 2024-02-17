5470934dfb8aeb753233c7b081b8cb1a163d0652
Ani-1709 <124597472+Ani-1709@users.noreply.github.com>
2024-02-17 22:25:25 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "animesh@goggle.com"
+var accountPasssword = "12345"
+accountCity = "Jaipur"
+//accountId =234// not allowed const is fixed value
+accountEmail= "khela@gogle.com"
+accountCity="BBSR"
+accountPasssword= "78910"
+let accountState;
+console.log(accountId);
+/*prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountEmail,accountId,accountPasssword,accountCity,accountState])
