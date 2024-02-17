eb50f9038c56002a1039674c8b897556f0b9f6d6
Ani-1709 <124597472+Ani-1709@users.noreply.github.com>
2024-02-17 22:43:55 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all JS code as newer version
+//alert("hello") // we are using node js, not browser
+// number=> 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value its type is object
+// undefined => its type is undefined
+// symbol => unique
+// object 
+let name="animesh"
+console.log(typeof "name")
