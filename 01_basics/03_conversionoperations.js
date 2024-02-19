ca4d8cef25e5dfda68cc19bd9626e8cb60ce8855
Ani-1709 <124597472+Ani-1709@users.noreply.github.com>
2024-02-19 19:58:46 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,21 @@
+let score="33abc"
+console.log(typeof score)
+console.log(typeof (score))
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+// truue => 1; false =>0
+let isLoggedIn =1
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+console.log(typeof(booleanIsLoggedIn));
+// 1=> true; 0=> false
+// "hitesh"==>true
+// "" empty string =>false
+let someNumber=33;
+let stringNum=String(someNumber)
+console.log(stringNum)
+console.log(typeof stringNum);
